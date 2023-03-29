d19573c2bc80ce597dd67b6a82903bea7a2383d6
Máximo Canedo <30154899delacroix@gmail.com>
2023-03-29 16:00:46 -0300

    Comienzo de la resolución del 2do ejercicio

--- Ejercicio2.Designer.cs
+++ Ejercicio2.Designer.cs
@@ -30,20 +30,202 @@ namespace TrabajoPractico1
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ejercicio2));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.gbIngresoDatos = new System.Windows.Forms.GroupBox();
+            this.gbElementos = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.lbNombres = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.lbApellido = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbApellido = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.gbIngresoDatos.SuspendLayout();
+            this.gbElementos.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.gbIngresoDatos, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.gbElementos, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(384, 261);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // gbIngresoDatos
+            // 
+            this.gbIngresoDatos.Controls.Add(this.tableLayoutPanel3);
+            this.gbIngresoDatos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbIngresoDatos.Location = new System.Drawing.Point(8, 8);
+            this.gbIngresoDatos.Margin = new System.Windows.Forms.Padding(8);
+            this.gbIngresoDatos.Name = "gbIngresoDatos";
+            this.gbIngresoDatos.Size = new System.Drawing.Size(176, 245);
+            this.gbIngresoDatos.TabIndex = 0;
+            this.gbIngresoDatos.TabStop = false;
+            this.gbIngresoDatos.Text = "Ingreso de datos";
+            // 
+            // gbElementos
+            // 
+            this.gbElementos.Controls.Add(this.tableLayoutPanel2);
+            this.gbElementos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbElementos.Location = new System.Drawing.Point(200, 8);
+            this.gbElementos.Margin = new System.Windows.Forms.Padding(8);
+            this.gbElementos.Name = "gbElementos";
+            this.gbElementos.Size = new System.Drawing.Size(176, 245);
+            this.gbElementos.TabIndex = 1;
+            this.gbElementos.TabStop = false;
+            this.gbElementos.Text = "Elementos";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.btnBorrar, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.lbNombres, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 16);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(170, 226);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnBorrar.Location = new System.Drawing.Point(47, 196);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 0;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            // 
+            // lbNombres
+            // 
+            this.lbNombres.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbNombres.FormattingEnabled = true;
+            this.lbNombres.Location = new System.Drawing.Point(3, 3);
+            this.lbNombres.Name = "lbNombres";
+            this.lbNombres.Size = new System.Drawing.Size(164, 184);
+            this.lbNombres.Sorted = true;
+            this.lbNombres.TabIndex = 1;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.lbNombre, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.lbApellido, 0, 3);
+            this.tableLayoutPanel3.Controls.Add(this.tbNombre, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.tbApellido, 0, 4);
+            this.tableLayoutPanel3.Controls.Add(this.btnAgregar, 0, 5);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 16);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 7;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 66.66666F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(170, 226);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Location = new System.Drawing.Point(3, 28);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(44, 13);
+            this.lbNombre.TabIndex = 0;
+            this.lbNombre.Text = "Nombre";
+            // 
+            // lbApellido
+            // 
+            this.lbApellido.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lbApellido.AutoSize = true;
+            this.lbApellido.Location = new System.Drawing.Point(3, 88);
+            this.lbApellido.Name = "lbApellido";
+            this.lbApellido.Size = new System.Drawing.Size(44, 13);
+            this.lbApellido.TabIndex = 1;
+            this.lbApellido.Text = "Apellido";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbNombre.Location = new System.Drawing.Point(3, 50);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(164, 20);
+            this.tbNombre.TabIndex = 2;
+            // 
+            // tbApellido
+            // 
+            this.tbApellido.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbApellido.Location = new System.Drawing.Point(3, 110);
+            this.tbApellido.Name = "tbApellido";
+            this.tbApellido.Size = new System.Drawing.Size(164, 20);
+            this.tbApellido.TabIndex = 3;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAgregar.Location = new System.Drawing.Point(47, 149);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // Ejercicio2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(400, 300);
             this.Name = "Ejercicio2";
             this.Text = "Segundo Ejercicio · TP Nº 1 PROG III";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.gbIngresoDatos.ResumeLayout(false);
+            this.gbElementos.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox gbIngresoDatos;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.Label lbApellido;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbApellido;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.GroupBox gbElementos;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.ListBox lbNombres;
     }
 }
--- Ejercicio2.cs
+++ Ejercicio2.cs
@@ -15,6 +15,59 @@ namespace TrabajoPractico1
         public Ejercicio2()
         {
             InitializeComponent();
+            /*// Para probar el programa
+            this.lbNombres.Items.Add("Máximo Canedo");
+            this.lbNombres.Items.Add("Luis García");
+            this.lbNombres.Items.Add("Carmen Itulaín");
+            //*/
+        }
+        private bool verificarNombre()
+        {
+            return (this.tbNombre.Text.Trim() != "");
+        }
+        private bool verificarApellido()
+        {
+            return (this.tbApellido.Text.Trim() != "");
+        }
+
+        private bool existeEnLaLista(string nombre, string apellido)
+        {
+            bool existe = false;
+            for (int i = 0; i < lbNombres.Items.Count; i++)
+            {
+                string aux = lbNombres.Items[i].ToString();
+                string n = nombre.Trim().ToUpper() + " " + apellido.Trim().ToUpper();
+                string x = aux.Trim().ToUpper();
+                // MessageBox.Show("n=" + n + "\r\nx=" + x);
+                // No me fue posible hacer .Trim() a 'aux'. Me sale siempre con el espacio en medio, así que por eso le agregué un espacio entre 'nombre' y 'apellido' para poder comparar.
+                if (x.Trim().ToUpper() == n)
+                {
+                    existe = true;
+                }
+            }
+            return existe;
+        }
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            bool nombreOK = verificarNombre();
+            bool apellidoOK = verificarApellido();
+
+            if(!nombreOK || !apellidoOK)
+            {
+                MessageBox.Show("¡El nombre y/o el apellido están en blanco!", "Atención");
+            }
+            else
+            {
+                // Nos fijamos que el nombre y apellidos no existan en la lista.
+                if(existeEnLaLista(this.tbNombre.Text, this.tbApellido.Text))
+                {
+                    MessageBox.Show("El nombre y apellido ya existen en la lista.");
+                } else
+                {
+                    // No existen el nombre y apellido, así que procedemos a agregarlos a la lista.
+                    lbNombres.Items.Add(this.tbNombre.Text + " " + this.tbApellido.Text);
+                }
+            }
         }
     }
 }
