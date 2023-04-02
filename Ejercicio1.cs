0cc6befaed9c24a6a8c09eb092f3346d0adf893e
Máximo Canedo <30154899delacroix@gmail.com>
2023-04-02 17:44:31 -0300

    Optimización final Ej. 1

--- Ejercicio1.cs
+++ Ejercicio1.cs
@@ -11,6 +11,11 @@ namespace TrabajoPractico1 {
         public Ejercicio1() {
             InitializeComponent();
         }
+        /// <summary>
+        /// Normaliza un string, con el fin de que sea posible poder compararlo con otros.  
+        /// </summary>
+        /// <param name="str">El string que se desea normalizar</param>
+        /// <returns>Un string sin acentos, en mayúsculas, ni espacios en blanco.</returns>
         private string normalizarString(string str) {
             string paso1 = new string(str
                 .Normalize(NormalizationForm.FormD)
@@ -21,6 +26,11 @@ namespace TrabajoPractico1 {
             string paso4 = paso3.Replace(" ", ""); // Cortar espacios, definitivamente
             return paso4;
         }
+        /// <summary>
+        /// Busca en @lbNombres1 y @lbNombres2 si hay algún ítem repetido.
+        /// </summary>
+        /// <param name="nombre">El string dado, del cual se quiere saber si está repetido en las listas.</param>
+        /// <returns>True, si hay un ítem ídentico al string dado. False, si no lo hay.</returns>
         private bool NombreEstaRepetido(string nombre) {
             string nombreNormalizado = normalizarString(nombre);
             foreach (string nombreList1 in this.lbNombres1.Items) {
@@ -35,22 +45,27 @@ namespace TrabajoPractico1 {
             }
             return false;
         }
-
-        private void button1_Click(object sender, EventArgs e) {
-            if (tbNombre.Text.Trim() == "") {
+        /// <summary>
+        /// Examina el nombre dado por el usuario y si no está en blanco, no está repetido en ninguna de las listas, lo agrega a la @lbNombres1
+        /// </summary>
+        private void AgregarItem() {
+            string nombreSinEspacios = tbNombre.Text.Trim();
+            if (nombreSinEspacios == "") {
                 MessageBox.Show("Debe ingresar un nombre");
                 return;
             }
-            if (NombreEstaRepetido(tbNombre.Text.Trim())) {
+            if (NombreEstaRepetido(nombreSinEspacios)) {
                 MessageBox.Show("Nombre repetido.", "Error");
             }
             else {
-                lbNombres1.Items.Add(tbNombre.Text.Trim());
+                lbNombres1.Items.Add(nombreSinEspacios);
                 tbNombre.Text = "";
             }
         }
-
-        private void button2_Click_1(object sender, EventArgs e) {
+        /// <summary>
+        /// De haber un ítem seleccionado por el usuario en la @lbNombres1, lo mueve a la @lbNombres2.
+        /// </summary>
+        private void MoverItemDeLista1ALista2() {
             if (lbNombres1.SelectedIndex >= 0) {
                 lbNombres2.Items.Add(lbNombres1.SelectedItem);
                 lbNombres1.Items.RemoveAt(lbNombres1.SelectedIndex);
@@ -59,9 +74,10 @@ namespace TrabajoPractico1 {
                 MessageBox.Show("No hay ningun nombre seleccionado.");
             }
         }
-
-        // Esta función mueve todos los elementos de @lbNombres1 a @lbNombres2.
-        private void button3_Click_1(object sender, EventArgs e) {
+        /// <summary>
+        /// Mueve todos los ítems de la @lbNombres1 a la @lbNombres2.
+        /// </summary>
+        private void MoverTodosLosNombresDeLista1aLista2() {
             if (lbNombres1.Items.Count != 0) {
                 foreach (string item in lbNombres1.Items) {
                     lbNombres2.Items.Add(item);
@@ -71,6 +87,15 @@ namespace TrabajoPractico1 {
             else {
                 MessageBox.Show("La lista izquierda se encuentra vacía.");
             }
+        }
+        private void button1_Click(object sender, EventArgs e) {
+            AgregarItem();
+        }
+        private void button2_Click_1(object sender, EventArgs e) {
+            MoverItemDeLista1ALista2();
+        }
+        private void button3_Click_1(object sender, EventArgs e) {
+            MoverTodosLosNombresDeLista1aLista2();
         }
     }
 }
