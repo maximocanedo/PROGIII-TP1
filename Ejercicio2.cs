06787a682bcd84bbf35f822cfc885a20004daa71
Máximo Canedo <30154899delacroix@gmail.com>
2023-04-01 21:46:30 -0300

    Optimización segundo ejercicio

--- Ejercicio2.cs
+++ Ejercicio2.cs
@@ -1,20 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
-namespace TrabajoPractico1
-{
-    public partial class Ejercicio2 : Form
-    {
-        public Ejercicio2()
-        {
+namespace TrabajoPractico1 {
+    public partial class Ejercicio2 : Form {
+        public Ejercicio2() {
             InitializeComponent();
             /*// Para probar el programa
             this.lbNombres.Items.Add("Máximo Canedo");
@@ -22,11 +15,9 @@ namespace TrabajoPractico1
             this.lbNombres.Items.Add("Carmen Itulaín");
             //*/
         }
-        private bool estaVacio(string str) 
-        { return str.Trim() != ""; }
+        private bool estaVacio(string str) { return str.Trim() != ""; }
 
-        private string normalizarString(string str)
-        {
+        private string normalizarString(string str) {
             string paso1 = new string(str
                 .Normalize(NormalizationForm.FormD)
                 .Where(c => char.GetUnicodeCategory(c) != System.Globalization.UnicodeCategory.NonSpacingMark)
@@ -35,80 +26,46 @@ namespace TrabajoPractico1
             string paso3 = paso2.Trim(); // Cortar espacios
             string paso4 = paso3.Replace(" ", ""); // Cortar espacios, definitivamente
             return paso4;
-        }
-        private bool existeEnLaLista(string nombre, string apellido)
-        {
+        } // Optimizado
+        private bool existeEnLaLista(string nombre, string apellido) {
             string nombreNormalizado = normalizarString(nombre);
             string apellidoNormalizado = normalizarString(apellido);
             string aux = nombreNormalizado + apellidoNormalizado;
-            foreach (string nombreList1 in this.lbNombres.Items)
-            {
-                if (normalizarString(nombreList1) == aux)
-                {
+            foreach (string nombreList1 in this.lbNombres.Items) {
+                if (normalizarString(nombreList1) == aux) {
                     return true;
                 }
-            }         
-            return false;
-        }
-        /*private bool existeEnLaLista(string nombre, string apellido)
-        {
-            bool existe = false;
-            for (int i = 0; i < lbNombres.Items.Count; i++)
-            {
-                string aux = lbNombres.Items[i].ToString();
-                string n = nombre.Trim().ToUpper() + " " + apellido.Trim().ToUpper();
-                string x = aux.Trim().ToUpper();
-                // MessageBox.Show("n=" + n + "\r\nx=" + x);
-                // No me fue posible hacer .Trim() a 'aux'. Me sale siempre con el espacio en medio, así que por eso le agregué un espacio entre 'nombre' y 'apellido' para poder comparar.
-                if (x.Trim().ToUpper() == n)
-                {
-                    existe = true;
-                }
             }
-            return existe;
-        }*/
-        private void btnAgregar_Click(object sender, EventArgs e)
-        {
+            return false;
+        } // Optimizado
+        private void btnAgregar_Click(object sender, EventArgs e) {
             bool nombreOK = estaVacio(tbNombre.Text);
             bool apellidoOK = estaVacio(tbApellido.Text);
-            
-
-            if(!nombreOK || !apellidoOK)
-            {
+            if (!nombreOK || !apellidoOK) {
                 MessageBox.Show("¡El nombre y/o el apellido están en blanco!", "Atención");
+                return;
             }
-            else
-            {
-                // Nos fijamos que el nombre y apellidos no existan en la lista.
-                if(existeEnLaLista(this.tbNombre.Text, this.tbApellido.Text))
-                {
-                    MessageBox.Show("El nombre y apellido ya existen en la lista.");
-                } else
-                {
-                    // No existen el nombre y apellido, así que procedemos a agregarlos a la lista.
-                    lbNombres.Items.Add(this.tbNombre.Text.Trim() + " " + this.tbApellido.Text.Trim());
-                }
+            if (existeEnLaLista(this.tbNombre.Text, this.tbApellido.Text)) {
+                MessageBox.Show("El nombre y apellido ya existen en la lista.");
+                return;
             }
+            lbNombres.Items.Add(this.tbNombre.Text.Trim() + " " + this.tbApellido.Text.Trim());
             tbNombre.Text = "";
             tbApellido.Text = "";
+        } // Optimizado
+
+        private void lbNombres_SelectedIndexChanged(object sender, EventArgs e) {
+
         }
 
-        private void lbNombres_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnBorrar_Click(object sender, EventArgs e)
-        {
-            if (lbNombres.SelectedItems.Count != 0)
-            {
+        private void btnBorrar_Click(object sender, EventArgs e) {
+            if (lbNombres.SelectedItems.Count != 0) {
                 lbNombres.Items.Remove(lbNombres.SelectedItem);
             }
-            else
-            {
+            else {
                 MessageBox.Show("Debe seleccionar un nombre.");
             }
-        }
+        } // Optimizado
 
         private void tbNombre_TextChanged(object sender, EventArgs e) {
 
