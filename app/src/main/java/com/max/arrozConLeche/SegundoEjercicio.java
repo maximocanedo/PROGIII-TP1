c7bcbab6bd8df9803e617e0ac09dfc5b80ae8846
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-04-26 16:10:09 -0300

    Resolución tercer ejercicio

--- app/src/main/java/com/max/arrozConLeche/SegundoEjercicio.java
+++ app/src/main/java/com/max/arrozConLeche/SegundoEjercicio.java
@@ -2,13 +2,64 @@ package com.max.arrozConLeche;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SegundoEjercicio extends AppCompatActivity {
-
+    private String getComparableString(String str) {
+        String normalized = str.replaceAll("[\\s\\p{Punct}]", "");
+        normalized = normalized
+                .replaceAll("[áÁ]", "A")
+                .replaceAll("[éÉ]", "E")
+                .replaceAll("[íÍ]", "I")
+                .replaceAll("[óÓ]", "O")
+                .replaceAll("[úÚ]", "U");
+        normalized = normalized.replaceAll("\\d+", "");
+        normalized = normalized.toUpperCase();
+        return normalized;
+    }
+    private EditText txtNombre, txtApellido;
+    private Button btnAgregar, btnBorrar;
+    private ListView l1;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segundo_ejercicio);
+        txtNombre = findViewById(R.id.ej2txtNombre);
+        txtApellido = findViewById(R.id.ej2txtApellido);
+        btnAgregar = findViewById(R.id.ej2btnAgregar);
+        btnBorrar = findViewById(R.id.ej2btnBorrar);
+        l1 = findViewById(R.id.ej2listView1);
+        List<String> elementos = new ArrayList<String>();
+
+        MyAdapter adapter = new MyAdapter(this, elementos);
+        l1.setAdapter(adapter);
+
+        btnAgregar.setOnClickListener(view -> {
+            String nombre = txtNombre.getText().toString();
+            String apellido = txtApellido.getText().toString();
+            String n_c = getComparableString(nombre);
+            String a_c = getComparableString(apellido);
+            if(n_c == "" || a_c == "") {
+                txtNombre.setError((n_c=="")?"Ingrese un valor válido": null);
+                txtApellido.setError((a_c=="")?"Ingrese un valor válido": null);
+            } else {
+                String finalName = nombre + " " + apellido;
+                adapter.add(finalName);
+            }
+        });
+        btnBorrar.setOnClickListener(view -> {
+            List<String> vals = adapter.getValues();
+            for(int i = 0; i < vals.size(); i++) {
+                adapter.remove(vals.get(i));
+            }
+        });
     }
 }
--- app/src/main/java/com/max/arrozConLeche/TercerEjercicio.java
+++ app/src/main/java/com/max/arrozConLeche/TercerEjercicio.java
@@ -1,14 +1,74 @@
 package com.max.arrozConLeche;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TercerEjercicio extends AppCompatActivity {
+
+    // Controles
+    private RadioGroup sexo, estadoCivil;
+    private CheckBox dataEntry, operadorPC, programador, reparadorPC, tester;
+    private Button btnMostrarSeleccionado;
+    private TextView txtResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tercer_ejercicio);
+        sexo = findViewById(R.id.ej3radioGroupSexo);
+        estadoCivil = findViewById(R.id.ej3radioGroupEstadoCivil);
+        btnMostrarSeleccionado = findViewById(R.id.btnMostrarSeleccionado);
+        txtResultado = findViewById(R.id.ej3txtResultado);
+        dataEntry = findViewById(R.id.ej3cbDataEntry);
+        operadorPC = findViewById(R.id.ej3cbOperadorPC);
+        programador = findViewById(R.id.ej3cbProgramador);
+        reparadorPC = findViewById(R.id.ej3cbReparadorPC);
+        tester = findViewById(R.id.ej3cbTester);
+
+        List<String> oficios = new ArrayList<String>();
+
+        btnMostrarSeleccionado.setOnClickListener(view -> {
+            String sexoSeleccionado = ((RadioButton)findViewById(sexo.getCheckedRadioButtonId())).getText().toString();
+            String estadoCivilSeleccionado=((RadioButton)findViewById(estadoCivil.getCheckedRadioButtonId())).getText().toString();
+
+            String resultado = "Seleccionaste: \n";
+            resultado += "Sexo: " + sexoSeleccionado + "\n";
+            resultado += "Estado Civil: " + estadoCivilSeleccionado + "\n";
+            resultado += "Oficio/s: \n";
+            for(int i = 0; i < oficios.size(); i++) {
+                resultado += " · " + oficios.get(i) + "\n";
+            }
+            txtResultado.setText(resultado);
+        });
+        // Listener para todos los checkboxes
+        CompoundButton.OnCheckedChangeListener checkboxListener = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    oficios.add(buttonView.getText().toString());
+                } else {
+                    oficios.remove(buttonView.getText().toString());
+                }
+            }
+        };
+        dataEntry.setOnCheckedChangeListener(checkboxListener);
+        operadorPC.setOnCheckedChangeListener(checkboxListener);
+        programador.setOnCheckedChangeListener(checkboxListener);
+        reparadorPC.setOnCheckedChangeListener(checkboxListener);
+        tester.setOnCheckedChangeListener(checkboxListener);
+
     }
 }
