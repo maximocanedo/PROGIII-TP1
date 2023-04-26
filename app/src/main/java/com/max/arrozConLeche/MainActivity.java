1418c8ba0b733e5c737a67b6d561cf0cb6d7980d
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-04-26 10:54:10 -0300

    Agregado de activities y funcionalidad a buttons de la MainActivity

--- app/src/main/java/com/max/arrozConLeche/MainActivity.java
+++ app/src/main/java/com/max/arrozConLeche/MainActivity.java
@@ -1,30 +1,39 @@
 package com.max.arrozConLeche;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnPrimerEjercicio, btnSegundoEjercicio, btnTercerEjercicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Controles
-        Button btnPrimerEjercicio = findViewById(R.id.btnPrimerEjercicio)
-             , btnSegundoEjercicio = findViewById(R.id.btnSegundoEjercicio)
-             , btnTercerEjercicio = findViewById(R.id.btnTercerEjercicio);
+        btnPrimerEjercicio = findViewById(R.id.btnPrimerEjercicio);
+        btnSegundoEjercicio = findViewById(R.id.btnSegundoEjercicio);
+        btnTercerEjercicio = findViewById(R.id.btnTercerEjercicio);
 
         btnPrimerEjercicio.setOnClickListener(view -> {
-
+            // Crear un Intent para ir a la actividad deseada
+            Intent intent = new Intent(MainActivity.this, PrimerEjercicio.class);
+            startActivity(intent);
         });
         btnSegundoEjercicio.setOnClickListener(view -> {
-
+            Intent intent = new Intent(MainActivity.this, SegundoEjercicio.class);
+            startActivity(intent);
         });
         btnTercerEjercicio.setOnClickListener(view -> {
-
+            Intent intent = new Intent(MainActivity.this, TercerEjercicio.class);
+            startActivity(intent);
         });
     }
 }
--- app/src/main/java/com/max/arrozConLeche/PrimerEjercicio.java
+++ app/src/main/java/com/max/arrozConLeche/PrimerEjercicio.java
@@ -1,11 +1,12 @@
 package com.max.arrozConLeche;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import android.app.ActionBar;
 import android.os.Bundle;
 
 public class PrimerEjercicio extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
